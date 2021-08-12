15f309b7e216c265edb0f894dd1ca1b67634dbeb
Marc Rufer <marc.rufer@isolutions.ch>
2021-08-12 09:17:05 +0200

    Set up Blazor Webassembly project  & Configure Authentication
    resolves #21
    resolves #17

---
+++ src/be.rufer.CS.Examples.Blazor.Webassembly/Client/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace be.rufer.CS.Examples.Blazor.Webassembly.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddHttpClient("be.rufer.CS.Examples.Blazor.Webassembly.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("be.rufer.CS.Examples.Blazor.Webassembly.ServerAPI"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://0c0a99b7-bce0-4f60-8522-1e3333fd1453/API.Access");
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
